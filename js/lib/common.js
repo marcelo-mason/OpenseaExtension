397f7657a295d1320c3f6ac5d40daa17cd025a92
donc310 <gigzblog@gmail.com>
2021-08-16 15:38:29 +0100

    Major fixes

--- js/lib/common.js
+++ js/lib/common.js
@@ -31,23 +31,24 @@ var parseQuery = (function (d, x, params, p, i, j) {
  *
  * @param {Number} ms
  */
- function sleep(ms) {
+function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 /**
  * 
  * @param {*} key 
+ * @param {*} _d 
  * @returns 
  */
- async function getLocalStorage(key) {
+async function getLocalStorage(key, _d = null) {
     return new Promise((resolve, reject) => {
         chrome.storage.local.get(key, items => {
             try {
                 if (key in items) {
                     resolve(items[key])
                 }
-                resolve(null)
+                resolve(_d)
             } catch (error) {
                 reject(error)
             }
@@ -68,4 +69,27 @@ async function setLocalStorage(key, value) {
             resolve(true)
         });
     })
+}
+
+/**
+ * 
+ * @returns 
+ */
+async function loadCollections() {
+    let response = await fetch("https://collections.rarity.tools/static/collections.json", {
+        "headers": {
+            "accept": "application/json, text/plain, */*",
+            "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
+            "sec-ch-ua-mobile": "?0"
+        },
+        "referrer": "https://rarity.tools/",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "omit"
+    })
+    response = await response.json()
+    setLocalStorage('collections', response)
+    return response
 }
